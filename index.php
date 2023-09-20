9c72f200c1261b04c388d6f514c41422bc8ad8dc
vlabannister <vbannister@wisc.edu>
2023-09-19 21:46:40 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<title>Victoria amp&; Karen</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<style>
+		body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+	
+		body, html {
+	  	height: 100%;
+	  	line-height: 1.8;
+		}
+	
+		/* Full height image header */
+		.bgimg-1 {
+	  	background-position: center;
+	  	background-size: cover;
+	  	background-image: url("/w3images/mac.jpg");
+	  	min-height: 100%;
+		}
+	
+		.w3-bar .w3-button {
+	 	 padding: 16px;
+		}
+		</style>
+	</head>
+
+<nav>
+	<a href="html">Title Page</a>
+	<a href=INSERT LOCAL iJUSITINE>iJustine</a>
+	<a href=INSERT LOCAL NANCY DOUYON>Nancy Douyon</a>
+</nav>
+
+	<body>
+		<h1>Victoria Bannister & Karen Horns</h1>
+
+		<h2>Assignment #2</h2>
+	
+		<h3>A Little About Victoria Bannister</h3>
+			<img src="GET A JPEG OF VICTORIA" width="##" height="##" alt="Victoria Bannister">
+		<p>Put in some intro text about Victoria</p>
+		<h4>Victoria's Tech Hero: iJustine</h4>
+			<p><a href=https://www.youtube.com/@iJustine>iJustine YouTube Channel</a></p>
+			<p><a id=WEBSITE PAGE>Go To iJustine Page</a></p>
+
+		<h3>A Little About Karen Horns</h3>
+			<img src="GET A JPEG OF KAREN" width="##" height="##" alt="Karen Horns">
+			<p>Karen Horns is a Madison native currently living in Denver, CO. She got into librarianship through her love of books and a recommendation from an iSchool alum who sparked a love of metadata, data, and information in general. Karen is in her 2nd year of her MLIS degree, attending via the Distance Learning Program. She currently works in organizational management for a small commercial real estate firm.</p>
+		<h4>Karen's Tech Hero: Nancy Douyon</h4>
+			<p><a href=https://nancydouyon.com/>Nancy Douyon's Website</a></p>
+			<p><a id=WEBSITE PAGE>Go To Nancy Douyon Page</a></p>
+	
+	</body>
