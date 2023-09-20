98dbd787bb60c5bbb8711ebfb051f80f7a9afd49
vlabannister <vbannister@wisc.edu>
2023-09-19 21:54:32 -0500

    Add ijustine.php

---
+++ ijustine.php
@@ -0,0 +1,18 @@
+<!doctype HTML>
+
+<html lang="en">
+
+<meta charset="UTF-8">
+
+<head>
+<title> iJustine </title>
+<link rel="stylesheet" href="testsheet.css">
+</head>
+
+<h1> iJustine </h1>
+
+<img src="GET A JPEG OF IJUSTINE" width="##" height="##" alt="Justine Ezarik aka iJustine">
+
+<body>
+Justine Ezarik, more widely known as iJustine, is an American Youtuber and Tech Influencer. She started out as a content creator on YouTube in the late 2000s reviewing products such as the original iPhone. She branched out into more products and brands and built a multi-million following across YouTube and a variety of social media sites. Victoria admires that she proves that anyone can be tech-savvy, especially regardless of your gender. One of Victoria's favorite things that iJustine has done is to partner with Made With Code, an initiative by Google to introduce coding to middle school and high school girls and to encourage them to pursue STEAM education and careers.
+</body>
