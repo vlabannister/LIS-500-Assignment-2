7d0f79807edb9669ab7d7994f7f8cf571c739acc
vlabannister <vbannister@wisc.edu>
2023-09-19 21:51:31 -0500

    Formatted code, added style

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 
-	<head>
-		<title>Victoria amp&; Karen</title>
-		<meta charset="utf-8">
+<head>
+	<title>Victoria amp&; Karen</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-		<style>
-		body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
- 		{padding: 100px;}
-		</style>
-	</head>
+	<style>
+		body,
+		h1,
+		h2,
+		h3,
+		h4,
+		h5,
+		h6 {
+			font-family: "Raleway", sans-serif
+		}
+
+		body,
+		html {
+			height: 100%;
+			line-height: 1.8;
+		}
+
+		body {
+			padding: 100px;
+		}
+	</style>
+</head>
 
 <nav>
 	<a href="html">Title Page</a>
@@ -17,23 +37,30 @@
 	<a href=INSERT LOCAL NANCY DOUYON>Nancy Douyon</a>
 </nav>
 
-	<body>
-		<h1>Victoria Bannister & Karen Horns</h1>
+<body>
+	<h1>Victoria Bannister & Karen Horns</h1>
 
-		<h2>Assignment #2</h2>
-	
-		<h3>A Little About Victoria Bannister</h3>
-			<img src="GET A JPEG OF VICTORIA" width="##" height="##" alt="Victoria Bannister">
-		<p>Victoria Bannister lives in Madison, WI, and is a 4th year part-time student in the Master of Library and Information Studies (MLIS) Distance Learning Program. She currently works full-time at the Department of Public Instruction (DPI) on the Literacy and Mathematics team. She started at DPI in the Fall of 2019 and was inspired and encouraged by her new colleagues to apply to the MLIS program at UW-Madison, where she started in the Fall of 2020. Outside of work and school, Victoria enjoys spending time chasing after her toddler!</p>
-		<h4>Victoria's Tech Hero: iJustine</h4>
-			<p><a href=https://www.youtube.com/@iJustine>iJustine YouTube Channel</a></p>
-			<p><a id=WEBSITE PAGE>Go To iJustine Page</a></p>
+	<h2>Assignment #2</h2>
 
-		<h3>A Little About Karen Horns</h3>
-			<img src="GET A JPEG OF KAREN" width="##" height="##" alt="Karen Horns">
-			<p>Karen Horns is a Madison native currently living in Denver, CO. She got into librarianship through her love of books and a recommendation from an iSchool alum who sparked a love of metadata, data, and information in general. Karen is in her 2nd year of her MLIS degree, attending via the Distance Learning Program. She currently works in organizational management for a small commercial real estate firm.</p>
-		<h4>Karen's Tech Hero: Nancy Douyon</h4>
-			<p><a href=https://nancydouyon.com/>Nancy Douyon's Website</a></p>
-			<p><a id=WEBSITE PAGE>Go To Nancy Douyon Page</a></p>
-	
-	</body>
+	<h3>A Little About Victoria Bannister</h3>
+	<img src="GET A JPEG OF VICTORIA" width="##" height="##" alt="Victoria Bannister">
+	<p>Victoria Bannister lives in Madison, WI, and is a 4th year part-time student in the Master of Library and
+		Information Studies (MLIS) Distance Learning Program. She currently works full-time at the Department of Public
+		Instruction (DPI) on the Literacy and Mathematics team. She started at DPI in the Fall of 2019 and was inspired
+		and encouraged by her new colleagues to apply to the MLIS program at UW-Madison, where she started in the Fall
+		of 2020. Outside of work and school, Victoria enjoys spending time chasing after her toddler!</p>
+	<h4>Victoria's Tech Hero: iJustine</h4>
+	<p><a href=https://www.youtube.com/@iJustine>iJustine YouTube Channel</a></p>
+	<p><a id=WEBSITE PAGE>Go To iJustine Page</a></p>
+
+	<h3>A Little About Karen Horns</h3>
+	<img src="GET A JPEG OF KAREN" width="##" height="##" alt="Karen Horns">
+	<p>Karen Horns is a Madison native currently living in Denver, CO. She got into librarianship through her love of
+		books and a recommendation from an iSchool alum who sparked a love of metadata, data, and information in
+		general. Karen is in her 2nd year of her MLIS degree, attending via the Distance Learning Program. She currently
+		works in organizational management for a small commercial real estate firm.</p>
+	<h4>Karen's Tech Hero: Nancy Douyon</h4>
+	<p><a href=https://nancydouyon.com />Nancy Douyon's Website</a></p>
+	<p><a id=WEBSITE PAGE>Go To Nancy Douyon Page</a></p>
+
+</body>
